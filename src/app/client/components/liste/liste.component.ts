6f375508b801dbfd088947cd8edf99802145d479
absrimka <absrimka65@gmail.com>
2022-07-28 16:08:18 +0000

    données

---
+++ src/app/client/components/liste/liste.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Produit } from 'src/app/shared/models/produits';
+
+@Component({
+  selector: 'app-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.css']
+})
+export class ListeComponent implements OnInit {
+  @Input() produits:Produit[]|undefined=undefined
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

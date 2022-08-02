23c4ae8a47726a616bcc150129cf4f441b78bfef
absrimka <absrimka65@gmail.com>
2022-08-02 14:25:01 +0000

    detail

--- src/app/client/pages/details/details.component.ts
+++ src/app/client/pages/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Catalogue } from 'src/app/shared/models/catalogue';
+import { Detail, Menu } from 'src/app/shared/models/details';
+import { CatalogueStoreService } from 'src/app/shared/services/catalogue-store.service';
 
 @Component({
   selector: 'abs-details',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  produits: any
+  // produits : Menu| null = null;
 
-  constructor() { }
+  constructor(private serv:CatalogueStoreService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    // this.id = this.route.snapshot.paramMap.get('id');
+    // this.type = this.route.snapshot.paramMap.get('type');
+    // alert("ok")
+        this.serv.produit$(1).subscribe((data)=>{
+           this.produits=data
+        });
+        console.log(this.produits)
   }
 
 }
